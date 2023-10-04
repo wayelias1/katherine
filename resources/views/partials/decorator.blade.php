a4b882c1d308bd02e84b7c0980ad8a132c979b16
wayelias1 <wayelias13@gmail.com>
2023-10-04 13:22:19 -0400

    acomode hero y añadi cosas en hero y añadi css en global

--- resources/views/partials/decorator.blade.php
+++ resources/views/partials/decorator.blade.php
@@ -1,3 +1,3 @@
-<div class="decorator is-flex is-align-items-center" style="width: 50%">
-    <span class="has-margin-right-10" style="border: 1px solid {{$color}}; width:100%"></span><i style="border: 2px solid {{$color}};aspect-ratio: 1/1; border-radius:100%; width:10px; height:10px"></i>
+<div class="decorator is-flex is-align-items-center is-half-width">
+    <span class="has-margin-right-10 is-full-width" style="border: 1px solid {{$color}};"></span><i style="border: 2px solid {{$color}};aspect-ratio: 1/1; border-radius:100%; width:10px; height:10px"></i>
 </div>
