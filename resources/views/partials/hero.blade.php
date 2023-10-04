3fbbc046bf1ce00fac72be3cbe29c930fa07ae2c
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-10-04 18:36:55 -0400

    responsive - done

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,12 +1,18 @@
-<div class="has-background-primary" style="height: 100vh;">
+<div style="height: 100vh">
     <div class="columns is-gapless is-full-height">
-      <div class="column is-6 is-full-height">
-        <div class="light is-full-width caja-1">
-          <a href="https://www.katherineboean.info"><img class="has-padding-left-20" src="@asset('images/logo.svg')" alt="logo" style="width: 40%; height:25%"></a>  
+
+      {{-- columna 1 --}}
+      <div class="column is-6">
+
+        {{-- primera division --}}
+        <div class="lets-talk has-background-primary has-padding-top-10 has-padding-left-40 has-padding-bottom-40 has-padding-right-40">
+          <a href="https://www.katherineboean.info">
+            <img src="@asset('images/logo.svg')" alt="logo" style="width: 40%; min-width: 160px;">
+          </a>  
 
           <div class="has-text-white columns m-0 is-mobile" style="height: 60%">
-            <div class="column is-1 has-padding-left-50-desktop has-padding-right-50-desktop has-padding-left-30-touch has-padding-right-30-touch">
-                <span class="is-flex is-justify-content-flex-end rotate-270deg" style="text-wrap: nowrap;">Financial Literacy Specialist</span>
+            <div class="column is-1 has-padding-left-10">
+              <span class="is-flex is-justify-content-flex-end rotate-270deg" style="text-wrap: nowrap;">Financial Literacy Specialist</span>
             </div>
             <div class="column is-8 is-offset-2-desktop is-offset-1-mobile" style="width: max-content">
               <span>Business Consultant</span>
@@ -17,21 +23,33 @@
                 </div>
             </div>
           </div>
-          <div class="column has-margin-bottom-50 has-margin-right-50 has-text-align-right">
+          <div class="column has-text-align-right">
             <span class="has-text-black" style="text-wrap: nowrap;">Schedule a meeting</span>
           </div>          
         </div>
 
-        <div class="is-full-width has-background-dark is-flex is-justify-content-space-between caja-2">
-          <div class="is-flex is-full-height" style="width:55%; background: url(@asset('images/Rectangle-image.svg'))center center/cover no-repeat; opacity:0.5">
+        {{-- segunda division --}}
+        <div class="services columns is-multiline is-gapless is-marginless has-background-dark">
+          <div class="column is-7 is-paddingless">
+            <div class="is-full-height" style="background: url(@asset('images/Rectangle-image.svg'))center center/cover no-repeat; opacity:0.5"></div>
           </div>
-          <div class="is-full-height has-text-white is-flex is-flex-direction-column is-align-content-center has-padding-20 is-clipped" style="width: 45%;">
-            <span class="is-size-3 is-1 has-text-white has-text-right has-padding-bottom-20-desktop m-0">01</span>
-            <h4>NXTVISOR</h4>
-            <div class="is-flex is-align-items-center is-half-width">
-              @include('partials.decorator', ['color'=>'#F2F2F2'])
+          <div class="column is-5 is-clipped" style="">
+            <div class="has-text-light has-padding-30 is-full-height is-flex is-flex-direction-column is-justify-content-space-between">
+              <p class="is-size-3 has-text-right">
+                01
+              </p>
+              <div>
+                <h4 class="is-size-4 has-text-light">
+                  NXTVISOR
+                </h4>
+                <div class="is-flex is-align-items-center is-half-width">
+                  @include('partials.decorator', ['color'=>'#F2F2F2'])
+                </div>
+                <p class="is-size-6">
+                  We guide around strengthening and implementing financial strategies for business owners, start-ups and enterprises.
+                </p>
+              </div>
             </div>
-            <p>We guide around strengthening and implementing financial strategies for business owners, start-ups and enterprises.</p>
           </div>
 
           {{-- <div class="is-full-height has-text-white is-flex is-flex-direction-column is-align-content-center has-padding-30" style="width: 45%;">
@@ -69,23 +87,31 @@
             </div>
             <p>Focused on real estate in the areas north of Atlanta.</p>
           </div> --}}
+          
         </div>
       </div>
 
-      <div class="column is-5">
-        <div style="height: calc(100vh - 100px)">
-          <img class="card-image is-full-height is-full-width" src="@asset('images/KATHERINE.svg')" alt="Katherine Boean" style="object-fit:cover">
+      {{-- columna 2 --}}
+      <div class="column is-5 has-background-white is-relative">
+        <div class="is-relative" style="background: url(@asset('images/KATHERINE.svg')) center center / cover no-repeat; height: 100vh;">
         </div>
-        <div class="has-background-white is-flex is-justify-content-space-around is-align-items-center is-full-width" style="height: 100px;">
-          <a class="has-text-black" href="tel: +1 404 777 5857">+1 404 777 5857</a>
-          <a class="has-text-black" href="https://www.google.com/maps/place/Atlanta,+Georgia,+EE.+UU./@33.7675434,-84.5849409,11z/data=!3m1!4b1!4m6!3m5!1s0x88f5045d6993098d:0x66fede2f990b630b!8m2!3d33.748752!4d-84.3876845!16zL20vMDEzeXE?hl=es&entry=ttu">Atlanta, EU</a>
+        <div class="is-flex is-justify-content-space-around is-align-items-center is-full-width has-background-white" 
+            style="height: 100px; position: absolute; bottom: 0;">
+          <a class="has-text-black" href="tel: +1 404 777 5857">
+            +1 404 777 5857
+          </a>
+          <a class="has-text-black" href="https://www.google.com/maps/place/Atlanta,+Georgia,+EE.+UU./@33.7675434,-84.5849409,11z/data=!3m1!4b1!4m6!3m5!1s0x88f5045d6993098d:0x66fede2f990b630b!8m2!3d33.748752!4d-84.3876845!16zL20vMDEzeXE?hl=es&entry=ttu">
+            Atlanta, EU
+          </a>
         </div>
       </div>
-      <div class="column is-1 caja-4">
-        <div class="has-background-dark is-flex is-justify-content-center is-align-items-center" style="height:15%;">
+
+      {{-- columna 3 --}}
+      <div class="navigation column is-1 has-background-primary">
+        <div class="navigation-plus has-padding-20 has-background-dark is-flex is-justify-content-center is-align-items-center">
           <div class="image is-48x48" style="background: url(@asset('images/plus-circle.svg'))center center/contain no-repeat"></div>
         </div>
-        <div class="is-flex is-flex-direction-column is-justify-content-space-around is-full-width has-text-align-center has-padding-bottom-100" style="height: 85%;">
+        <div class="items-row is-flex is-justify-content-space-around is-full-width has-text-align-center has-padding-30">
           <a class="" href="https://www.instagram.com/katherineboean/">Instagram</a>
           <a class="" href="email: hello@katherineboean.com">Email</a>
           <a class="" href="https://www.linkedin.com/in/katherine-boean/">Linkedin</a>
