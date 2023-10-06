ef12aaabe0f7391d3377308822ddcf82bdd74fd8
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-10-06 14:18:39 -0400

    little changes - alignment

--- resources/views/partials/decorator.blade.php
+++ resources/views/partials/decorator.blade.php
@@ -1,3 +1,6 @@
 <div class="decorator is-flex is-align-items-center is-half-width">
-    <span class="has-margin-right-10 is-full-width" style="border: 1px solid {{$color}};"></span><i style="border: 2px solid {{$color}};aspect-ratio: 1/1; border-radius:100%; width:10px; height:10px"></i>
+    <span class="has-margin-right-10 is-full-width" style="height: 1px; background-color: {{$color}};">
+    </span>
+        <i style="border: 2px solid {{$color}};aspect-ratio: 1/1; border-radius:100%; width:10px; height:10px">
+    </i>
 </div>
--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -5,11 +5,11 @@
       {{-- primera división --}}
       <div class="lets-talk has-background-primary has-padding-top-10 has-padding-left-40 has-padding-bottom-40 has-padding-right-40">
         <a href="https://www.katherineboean.info">
-          <img src="@asset('images/logo.svg')" alt="logo" style="width: 40%; min-width: 160px;">
+          <img src="@asset('images/logo.svg')" alt="logo" style="width: 32%; min-width: 200px;">
         </a>
         <div class="has-text-white columns m-0 is-mobile" style="height: 60%">
           <div class="column is-1 has-padding-left-10">
-            <span class="is-flex is-justify-content-flex-end rotate-270deg" style="text-wrap: nowrap;">Financial Literacy Specialist</span>
+            <span class="has-margin-top-20 is-flex is-justify-content-flex-end rotate-270deg" style="text-wrap: nowrap;">Financial Literacy Specialist</span>
           </div>
           <div class="column is-8 is-offset-2-desktop is-offset-1-mobile" style="width: max-content">
             <span>Business Consultant</span>
@@ -21,7 +21,7 @@
           </div>
         </div>
         <div class="has-text-align-right">
-          <span class="has-text-black" style="text-wrap: nowrap;">Schedule a meeting</span>
+          <a href="https://www.katherineboean.info" class="has-padding-right-20 has-text-hover-light" style="text-wrap: nowrap;">Schedule a meeting</a>
         </div>
       </div>
       {{-- segunda división --}}
@@ -80,12 +80,7 @@
               init() {
                 let carrusel = document.querySelector('#carrusel');
                 setInterval(() => {
-                  // setInterval(() => {
-                  //   carrusel.classList.add('carrusel');
-                  // }, 4000);
-                  //   carrusel.classList.remove('carrusel');
                     this.currentIndex = (this.currentIndex + 1) % this.information.length;
-                    
                   }, 4000);
                 }
               }
@@ -98,7 +93,7 @@
     {{-- columna 2 --}}
     <div class="column is-5 has-background-white is-relative">
       <div class="is-relative" style="background: url(@asset('images/KATHERINE.svg')) center center / cover no-repeat; height: 100vh;"></div>
-      <div class="is-flex is-justify-content-space-around is-align-items-center is-full-width has-background-white"
+      <div class="contacts is-flex is-justify-content-space-around is-align-items-center is-full-width has-background-white"
            style="height: 100px; position: absolute; bottom: 0;">
         <a class="has-text-hover-primary" href="tel: +1 404 777 5857">+1 404 777 5857</a>
         <a class="has-text-hover-primary" href="https://www.google.com/maps/place/Atlanta,+Georgia,+EE.+UU./@33.7675434,-84.5849409,11z/data=!3m1!4b1!4m6!3m5!1s0x88f5045d6993098d:0x66fede2f990b630b!8m2!3d33.748752!4d-84.3876845!16zL20vMDEzeXE?hl=es&entry=ttu">Atlanta, EU</a>
@@ -106,10 +101,10 @@
     </div>
     {{-- columna 3 --}}
     <div class="navigation column is-1 has-background-primary is-clipped">
-      <div class="navigation-plus has-padding-20 has-background-dark is-flex is-justify-content-center is-align-items-center">
-        <div class="image is-48x48" style="background: url(@asset('images/plus-circle.svg'))center center/contain no-repeat"></div>
-      </div>
-      <div class="items-row is-flex is-justify-content-space-around is-full-width has-text-align-center has-padding-top-30 has-padding-bottom-30">
+      <a href="https://www.katherineboean.info" class="navigation-plus has-padding-20 has-background-dark is-flex is-justify-content-center is-align-items-center">
+        <div class="plus-button image is-48x48" style="background: url(@asset('images/plus-circle.svg'))center center/contain no-repeat"></div>
+      </a>
+      <div class="items-row is-flex is-justify-content-space-around is-full-width has-text-align-center has-padding-top-30 has-padding-bottom-30-touch has-padding-bottom-100-desktop">
         <a class="has-text-hover-light" href="https://www.instagram.com/katherineboean/">Instagram</a>
         <a class="has-text-hover-light" href="email: hello@katherineboean.com">Email</a>
         <a class="has-text-hover-light" href="https://www.linkedin.com/in/katherine-boean/">Linkedin</a>
